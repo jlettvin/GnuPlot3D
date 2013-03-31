5093f86a03c7f8e2d145710fdc637c23e1b06658
Jonathan D. Lettvin <jlettvin@gmail.com>
2013-03-31 00:57:16 -0400

    License

--- GnuPlot3D.py
+++ GnuPlot3D.py
@@ -6,7 +6,7 @@ __author__     = "jlettvin"
 __maintainer__ = "jlettvin"
 __email__      = "jlettvin@gmail.com"
 __copyright__  = "Copyright(c) 2013 Jonathan D. Lettvin, All Rights Reserved"
-__license__    = "Trade Secret"
+__license__    = "GPLv3"
 __status__     = "Production"
 __version__    = "0.0.1"
 
@@ -14,6 +14,21 @@ __version__    = "0.0.1"
 GunuPlot3D.py
 
 Implementes a dynamic scientific 3D visualizer class.
+
+Copyright(c) 2013 Jonathan D. Lettvin, All Rights Reserved"
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 See __main__ for an example of use.
 
